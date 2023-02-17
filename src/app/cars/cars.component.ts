d717c9ad14aa9dafefeee2bad5e960c10c38693c
Anand-1912 <anandchandrasekaran1912@gmail.com>
2023-02-17 14:30:14 +0530

    string interpolation data binding
    string interpolation used for data binding

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.css']
+  styleUrls: ['./cars.component.css'],
 })
 export class CarsComponent {
-
+  pageTitle: string = 'Cars!';
 }
